eeaf74d4576c170bfecff0d42b0e406674d070f1
Savino Ordine <savino.ordine@subito.it>
2015-07-07 15:18:24 +0200

    first espresso webview method (not a test)

--- app/src/androidTest/java/com/savinoordine/espressoplayground/EspressoWebView.java
+++ app/src/androidTest/java/com/savinoordine/espressoplayground/EspressoWebView.java
@@ -1,7 +1,26 @@
 package com.savinoordine.espressoplayground;
 
-/**
- * Created by savino on 07/07/15.
- */
+
+import android.support.test.espresso.web.webdriver.Locator;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.web.sugar.Web.onWebView;
+import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static android.support.test.espresso.web.webdriver.DriverAtoms.webClick;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
 public class EspressoWebView {
+
+    /**
+     * Click id_of_view button on web view page
+     */
+    private void clickButtonOnWebView() {
+
+        onWebView()
+                .withElement(findElement(Locator.ID, "id_of_view"))
+                .perform(webClick());
+    }
 }
